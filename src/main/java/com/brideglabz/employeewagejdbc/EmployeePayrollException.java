ad128b2960470e68b6811712f4ddd9da32890dd4
usha-12 <usha12ahirwar@gmail.com>
2022-11-28 01:34:22 +0530

    add a new empl to the payroll

--- src/main/java/com/brideglabz/employeewagejdbc/EmployeePayrollException.java
+++ src/main/java/com/brideglabz/employeewagejdbc/EmployeePayrollException.java
@@ -3,6 +3,7 @@ package com.brideglabz.employeewagejdbc;
 public class EmployeePayrollException extends Exception {
         public EmployeePayrollException(String message) {
 
+
             super(message);
         }
 }
--- src/test/java/com/brideglabz/employeewagejdbc/LibraryTest.java
+++ src/test/java/com/brideglabz/employeewagejdbc/LibraryTest.java
@@ -7,6 +7,7 @@ public class LibraryTest {
         Library classUnderTest = new Library();
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
+
     private void assertTrue(String s, Object someLibraryMethod) {
     }
 }
